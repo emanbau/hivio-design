317e934d2786374d34764fa11cfc71b42acb04ad
Eman Bau <emanbautista410@gmail.com>
2021-07-25 19:32:29 -0400

    Services Content

---
+++ components/servicesCards/services.js
@@ -0,0 +1,32 @@
+export const services = [
+    {
+        service: 'Custom Software Development',
+        paragraph: 'We develop and design custom software solutions for your company or startup. We work with you using the newest technologies to deliver solutions that fit the needs of their users on any platform.',
+        list: ['Website Development', 'Mobile App Development', 'Single Page Applications & Progressive Web Apps']
+    },
+    {
+        service: 'Mobile App Development',
+        paragraph: 'We help you develop creative and innovative mobile apps that deliver a cutting edge experience to users. We can help you from any stage of product development, from design and creating an MVP to maintenance.',
+        list: ['Native App Development', 'Cross-Platform App Development', 'Mobile Web App Development']
+    },
+    {
+        service: 'UI/UX Design',
+        paragraph: 'We help design the user interface and user experience for your software to ensure an effective app for your users. Using innovative tools and design procedures, we make sure your software is top notch.',
+        list: ['Wireframes', 'Mockups', 'Prototyping']
+    },
+    {
+        service: 'IT Consulting',
+        paragraph: 'We ensure that your tech workflow is effective and optimized by implementing the newest and innovative technology to remove handicaps, points of headache, and help grow your business.',
+        list: ['System and Platform Integration', 'IoT Solutions', 'Project Management Consulting']
+    },
+    {
+        service: 'Application Testing & QA',
+        paragraph: 'We make sure that your custom software works the way it is meant to work. We use effective testing and quality assurance strategies to eliminate problems with your product to ensure the highest quality software.',
+        list: ['Multi-Stage Unit Testing', 'Functional & End to End Testing', 'Quality Assurance Consulting']
+    },
+    {
+        service: 'Application Services',
+        paragraph: 'We help connect and integrate software systems and services with the development and maintenance of your application solutions to make sure they are optimized and always up and running.',
+        list: ['API Integrations', 'Cloud Service Integration', 'Software Deployment']
+    }
+]
